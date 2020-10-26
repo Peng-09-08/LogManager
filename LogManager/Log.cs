0f606dec707512b9556541fc809874a031f6ca0c
Peng <clhs205081823@gmail.com>
2020-10-26 18:02:03 +0800

    New project

---
+++ LogManager/Log.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogManager
+{
+    internal class Log
+    {
+        /// <summary>
+        /// Log型態
+        /// </summary>
+        public enum LogType
+        {
+            /// <summary>
+            /// 一般資訊
+            /// </summary>
+            Info = 0,
+
+            /// <summary>
+            /// 警告資訊
+            /// </summary>
+            Warring = 1,
+
+            /// <summary>
+            /// 錯誤資訊
+            /// </summary>
+            Error = 2,
+
+            /// <summary>
+            /// 系統資訊
+            /// </summary>
+            System = 3,
+
+            /// <summary>
+            /// 例外處裡資訊
+            /// </summary>
+            Execption = 4,
+
+            /// <summary>
+            /// 生產流程資訊
+            /// </summary>
+            Produce = 5,
+        }
+
+        /// <summary>
+        /// Log資訊
+        /// </summary>
+        public class LogInfo
+        {
+            /// <summary>
+            /// 訊息
+            /// </summary>
+            public string msg;
+
+            /// <summary>
+            /// 型態
+            /// </summary>
+            public LogType type;
+        }
+
+        private static Queue<LogInfo> _LogQueue = new Queue<LogInfo>();
+
+        /// <summary>
+        /// 寫入Log
+        /// </summary>
+        /// <param name="msg">訊息</param>
+        public static void WriteLine(string msg)
+        {
+            Enqueue(LogType.Info, msg);
+        }
+
+        /// <summary>
+        /// 寫入Log
+        /// </summary>
+        /// <param name="type">型態</param>
+        /// <param name="msg">訊息</param>
+        public static void WriteLine(LogType type, string msg)
+        {
+            Enqueue(type, msg);
+        }
+
+        /// <summary>
+        /// 寫入Log
+        /// </summary>
+        /// <param name="format">format</param>
+        /// <param name="args">args</param>
+        public static void WriteLine(string format, params object[] args)
+        {
+            Enqueue(LogType.Info, string.Format(format, args));
+        }
+
+        /// <summary>
+        /// 寫入Log
+        /// </summary>
+        /// <param name="type">type</param>
+        /// <param name="format">format</param>
+        /// <param name="args">args</param>
+        public static void WriteLine(LogType type, string format, params object[] args)
+        {
+            Enqueue(type, string.Format(format, args));
+        }
+
+        /// <summary>
+        /// Log佇列
+        /// </summary>
+        /// <param name="type">型態</param>
+        /// <param name="msg">訊息</param>
+        private static void Enqueue(LogType type, string msg)
+        {
+            lock (_LogQueue)
+            {
+                LogInfo info = new LogInfo();
+                info.type = type;
+                info.msg = string.Format("{0} {1}", DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss]"), msg);
+
+                _LogQueue.Enqueue(info);
+            }
+
+#if _DEBUG
+            //Console.WriteLine(msg);
+#endif
+        }
+    }
+}
---
+++ LogManager/LogWriter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace LogManager
+{
+    /// <summary>
+    /// 將log訊息寫入至檔案
+    /// </summary>
+    public class LogWriter
+    {
+        private static StreamWriter _writer = null;
+        private static object _lockLog = new object();
+        private static Queue<string> _logBuffer = new Queue<string>();
+        private static string _logPath = string.Empty;
+        private static bool _isInitial = false;
+        private static bool _isRun = false;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="name">log檔名稱</param>
+        public static void Init(string name)
+        {
+            if (_isInitial)
+                return;
+
+            if (_logPath == string.Empty)
+            {
+                string timeStr = DateTime.Now.ToString("yyyyMMddHHmmss");
+                _logPath = Environment.CurrentDirectory + "Logs\\" + name + "_" + timeStr + ".txt";
+            }
+
+            FileInfo fileInfo = new FileInfo(_logPath);
+            DirectoryInfo dirInfo = new DirectoryInfo(fileInfo.DirectoryName);
+
+            // check dir exist
+            if (!dirInfo.Exists)
+                dirInfo.Create();
+
+            // delete previous log when open
+            if (File.Exists(_logPath))
+                File.Delete(_logPath);
+
+            // create log
+            using (FileStream fs = File.Create(_logPath))
+            {
+                fs.Close();
+            }
+
+            // create StreamWriter
+            if (_writer == null)
+                _writer = File.AppendText(_logPath);
+
+            _isInitial = true;
+
+            // create thread for process log
+            _isRun = true;
+            ThreadPool.QueueUserWorkItem(_writeLog);
+        }
+
+        /// <summary>
+        /// 關閉LogWriter
+        /// </summary>
+        public static void Close()
+        {
+            _isRun = false;
+            _logBuffer.Clear();
+            _writer.Close();
+        }
+
+        /// <summary>
+        /// 加入log訊息
+        /// </summary>
+        /// <param name="logMessage">訊息</param>
+        public static void WriteLog(string logMessage)
+        {
+            lock (_lockLog)
+            {
+                _logBuffer.Enqueue(logMessage);
+            }
+        }
+
+        private static void _writeLog(object state)
+        {
+            while (_isRun)
+            {
+                while (_logBuffer.Count <= 0)
+                    Thread.Sleep(10);
+
+                string logMessage = string.Empty;
+
+                lock (_lockLog)
+                {
+                    if (_logBuffer.Count > 1000)
+                        logMessage = "Fatal Error: Too many log string in queue!!";
+                    else if (_logBuffer.Count > 0)
+                        logMessage = _logBuffer.Dequeue();
+                }
+
+                // append log
+                if (_writer != null)
+                    _writer.WriteLine(logMessage);
+            }
+        }
+    }
+}
