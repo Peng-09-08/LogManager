0438e81c645c3620ed9accf52cd146cc490b3121
Peng <clhs205081823@gmail.com>
2021-03-15 08:58:15 +0800

    1. Remove Log.cs. 2. Modify LogWriter.cs.

--- LogManager/LogWriter.cs
+++ LogManager/LogWriter.cs
@@ -1,205 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace LogManager
 {
     /// <summary>
     /// 將log訊息寫入至檔案
     /// </summary>
-    public class LogWriter
+    public static class LogWriter
     {
-        private static StreamWriter _writer = null;
-        private static object _lockLog = new object();
-        private static Queue<LogInfo> _logBuffer = new Queue<LogInfo>();
-        private static string _logPath = string.Empty;
-        private static bool _isInitial = false;
+        private static string _logFilePath = string.Empty;
+        private static bool _isInitialed = false;
         private static bool _isRun = false;
-        private static Dictionary<LogType, string> _pairMessage = new Dictionary<LogType, string>()
-        {
-            { LogType.Info, "[Information]" },
-            { LogType.Warning, "[===Warning===]" },
-            { LogType.Error, "[####Error####]" },
-            { LogType.Exception, "[!!Exception!!]" },
-            { LogType.System, "[**System**]" },
-        };
+        private static StreamWriter _writer = null;
+
+        private static object _logLock = new object();
+        private static Queue<string> _logBuffer = new Queue<string>();
 
         /// <summary>
-        /// Log型態
+        /// Declare a delegate with string type argument
         /// </summary>
-        public enum LogType
+        /// <param name="message"></param>
+        public delegate void ShowLogDelegate(string message);
+        private static ShowLogDelegate _showLogHandler;
+
+        /// <summary>
+        /// Register callback function
+        /// </summary>
+        /// <param name="func">Function with string type argument</param>
+        public static void RegisterShowLogDelegate(ShowLogDelegate func)
         {
-            /// <summary>
-            /// 無指定型態
-            /// </summary>
-            None = -1,
-
-            /// <summary>
-            /// 一般資訊
-            /// </summary>
-            Info,
-
-            /// <summary>
-            /// 警告資訊
-            /// </summary>
-            Warning,
-
-            /// <summary>
-            /// 錯誤資訊
-            /// </summary>
-            Error,
-
-            /// <summary>
-            /// 例外處裡資訊
-            /// </summary>
-            Exception,
-
-            /// <summary>
-            /// 系統資訊
-            /// </summary>
-            System,
-
-            ///// <summary>
-            ///// 生產流程資訊
-            ///// </summary>
-            //Produce,
+            _showLogHandler = func;
         }
 
         /// <summary>
-        /// Log資訊
+        /// Write message to log file
         /// </summary>
-        public class LogInfo
+        /// <param name="message">Message from user</param>
+        public static void WriteLog(string message)
         {
-            /// <summary>
-            /// 訊息
-            /// </summary>
-            public string Message;
-
-            /// <summary>
-            /// Log型態
-            /// </summary>
-            public LogType Type;
-
-            /// <summary>
-            /// 建構式
-            /// </summary>
-            /// <param name="msg">訊息</param>
-            /// <param name="type">Log型態</param>
-            public LogInfo(string msg, LogType type)
-            {
-                Message = msg;
-                Type = type;
-            }
+            message = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "   " + message;
+            _enqueueLog(message);
+            if (_showLogHandler != null)
+                _showLogHandler(message);
         }
 
         /// <summary>
-        /// 初始化
+        /// Initial LogWriter
         /// </summary>
-        /// <param name="name">log檔名稱</param>
-        public static void Init(string name)
+        public static void Init()
         {
-            if (_isInitial)
+            if (_isInitialed)
                 return;
 
-            if (_logPath == string.Empty)
+            try
             {
-                string timeStr = DateTime.Now.ToString("yyyyMMddHHmmss");
-                _logPath = Environment.CurrentDirectory + "\\Logs\\" + name + "_" + timeStr + ".txt";
+                if (string.IsNullOrEmpty(_logFilePath))
+                {
+                    string timeStr = DateTime.Now.ToString("yyyyMMddHHmmss");
+                    _logFilePath = "Logs\\" + "Log" + timeStr + ".txt";
+                }
+
+                // get file info and dir info
+                FileInfo logFileInfo = new FileInfo(_logFilePath);
+                DirectoryInfo logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+
+                // check dir exist
+                if (!logDirInfo.Exists)
+                    logDirInfo.Create();
+
+                // delete old log when open
+                if (File.Exists(_logFilePath))
+                    File.Delete(_logFilePath);
+
+                // create log
+                if (!File.Exists(_logFilePath))
+                {
+                    using (FileStream fs = File.Create(_logFilePath))
+                        fs.Close();
+                }
+
+                // create StreamWriter
+                if (_writer == null)
+                {
+                    _writer = File.AppendText(_logFilePath);
+                    _writer.AutoFlush = true;
+                }
+
+                _isInitialed = true;
+
+                // create thread for process log
+                _isRun = true;
+                ThreadPool.QueueUserWorkItem(_writeLog);
             }
-
-            FileInfo fileInfo = new FileInfo(_logPath);
-            DirectoryInfo dirInfo = new DirectoryInfo(fileInfo.DirectoryName);
-
-            // check dir exist
-            if (!dirInfo.Exists)
-                dirInfo.Create();
-
-            // create StreamWriter
-            if (_writer == null)
+            catch (Exception e)
             {
-                _writer = new StreamWriter(_logPath, false);
-                _writer.AutoFlush = true;
+                //WriteLog("Exception Message: " + e.Message + "\n" + e.StackTrace.ToString());
+                MessageBox.Show(e.Message + "\n" + e.StackTrace.ToString(), "Exception Message");
             }
-
-            _isInitial = true;
-
-            // create thread for process log
-            _isRun = true;
-            ThreadPool.QueueUserWorkItem(_writeLog);
         }
 
         /// <summary>
-        /// 關閉LogWriter
+        /// Close LogWriter
         /// </summary>
         public static void Close()
         {
             _isRun = false;
-            _logBuffer.Clear();
-            if (_writer != null)
-                _writer.Close();
+            _writer.Close();
         }
 
-        /// <summary>
-        /// 寫入一般log訊息
-        /// </summary>
-        /// <param name="message">訊息</param>
-        public static void WriteLog(string message)
+        private static void _enqueueLog(string logMessage)
         {
-            lock (_lockLog)
+            lock (_logLock)
             {
-                _logBuffer.Enqueue(new LogInfo(message, LogType.None));
-            }
-        }
-
-        /// <summary>
-        /// 寫入information訊息
-        /// </summary>
-        /// <param name="message">訊息</param>
-        public static void WriteInfoLog(string message)
-        {
-            lock (_lockLog)
-            {
-                _logBuffer.Enqueue(new LogInfo(message, LogType.Info));
-            }
-        }
-
-        /// <summary>
-        /// 寫入警告訊息
-        /// </summary>
-        /// <param name="message">訊息</param>
-        public static void WriteWarningLog(string message)
-        {
-            lock (_lockLog)
-            {
-                _logBuffer.Enqueue(new LogInfo(message, LogType.Warning));
-            }
-        }
-
-        /// <summary>
-        /// 寫入錯誤訊息
-        /// </summary>
-        /// <param name="message">訊息</param>
-        public static void WriteErrorLog(string message)
-        {
-            lock (_lockLog)
-            {
-                _logBuffer.Enqueue(new LogInfo(message, LogType.Error));
-            }
-        }
-
-        /// <summary>
-        /// 寫入例外訊息
-        /// </summary>
-        /// <param name="message">訊息</param>
-        public static void WriteExceptionLog(string message)
-        {
-            lock (_lockLog)
-            {
-                _logBuffer.Enqueue(new LogInfo(message, LogType.Exception));
+                _logBuffer.Enqueue(logMessage);
             }
         }
 
@@ -208,27 +124,33 @@ namespace LogManager
             while (_isRun)
             {
                 while (_logBuffer.Count <= 0)
-                    Thread.Sleep(10);
-
-                lock (_lockLog)
                 {
-                    LogInfo info = null;
+                    Thread.Sleep(10);
+                }
+
+                string logMessage = string.Empty;
+
+                lock (_logLock)
+                {
                     if (_logBuffer.Count > 1000)
-                    {
-                        info.Message = "Fatal Error: Too many log string in queue!!";
-                        info.Type = LogType.System;
-                    }
+                        logMessage = "Fatal Error: Too many log string in queue!!";
                     else if (_logBuffer.Count > 0)
-                        info = _logBuffer.Dequeue();
+                        logMessage = _logBuffer.Dequeue();
+                }
 
-                    // append log
+                if (!_isInitialed)
+                    MessageBox.Show("Error: LogWriter didn't initialize!!");
+
+                try
+                {
                     if (_writer != null)
-                    {
-                        string str = string.Format("{0}   {1}   {2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
-                            _pairMessage[info.Type], info.Message);
-
-                        _writer.WriteLine(str);
-                    }
+                        _writer.WriteLine(logMessage);
+                }
+                catch (Exception e)
+                {
+                    //WriteLog("Exception Message: " + e.Message + "\n" + e.StackTrace.ToString());
+                    MessageBox.Show(e.Message + "\n" + e.StackTrace.ToString(), "Exception Message");
+                    return;
                 }
             }
         }
